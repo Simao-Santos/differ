3dd1f84fc61441247586fb725241bb0b28f45216
Andreia Gouveia <andiigouveia@hotmail.com>
2020-10-15 12:56:30 +0100

    Added starter Template

---
+++ my-app/src/Pages/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../CSS/HomePage.css';
+import Header from '../Components/Header';
+
+function HomePage() {
+  return (
+    <div className="App">
+      <Header />
+
+      <div className="MainContainer">
+        <p>hELLO</p>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
