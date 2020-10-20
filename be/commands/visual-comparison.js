aa15bcf53577367206fe09eb677dc53df5693f26
Filipe Barbosa <up201909573@fe.up.pt>
2020-10-20 18:58:38 +0100

    Update visual-comparison.js
    Refactored some code

--- be/commands/visual-comparison.js
+++ be/commands/visual-comparison.js
@@ -17,23 +17,28 @@ module.exports = {
 			const sec = String(today.getSeconds()).padStart(2, '0');
 			const millisec = String(today.getMilliseconds()).padStart(3, '0');
 		
-			const filename = year + "_" + mon + "_" + day + "_" + hour + "_" + min + "_" + sec + "_" + millisec;
+			const filename =
+				year + "_" + mon + "_" + day + "_" +
+				hour + "_" + min + "_" + sec + "_" + millisec;
 		
 			await new Pageres({delay: 2})
-				.src('data:text/html,' + firstPageContent, ['w3counter'], {filename: filename + "-1"})
-				.src('data:text/html,' + secondPageContent, ['w3counter'], {filename: filename + "-2"})
+				.src('data:text/html,' + firstPageContent, ['w3counter'], {filename: filename + "-before"})
+				.src('data:text/html,' + secondPageContent, ['w3counter'], {filename: filename + "-after"})
 				.dest("./shots")
 				.run();
 				
 			console.log('Finished generating screenshots!');
 			console.log('Comparing screenshots...');
 		
-			const img1 = PNG.sync.read(fs.readFileSync('./shots/' + filename + '-1.png'));
-			const img2 = PNG.sync.read(fs.readFileSync('./shots/' + filename + '-2.png'));
+			const img1 = PNG.sync.read(fs.readFileSync('./shots/' + filename + '-before.png'));
+			const img2 = PNG.sync.read(fs.readFileSync('./shots/' + filename + '-after.png'));
 			const {width, height} = img1;
 			const diff = new PNG({width, height});
 		
-			const diff_pixels = pixelmatch(img1.data, img2.data, diff.data, width, height, {threshold: 0.1, diffColorAlt: [0, 200, 0], alpha: 0.5});
+			const diff_pixels = 
+				pixelmatch(img1.data, img2.data, diff.data, width, height,
+					{threshold: 0.1, diffColorAlt: [0, 200, 0], alpha: 0.5});
+
 			fs.writeFileSync('./shots/' + filename + '-diff.png', PNG.sync.write(diff));
 		
 			console.log('Finished generating screenshots!');
