8459fcb5e13a29e2f0b5609f4775204fd005a4f3
AndreiaGouveia <andreiagouveia1@gmail.com>
2020-10-22 17:50:09 +0100

    Add sticky footer
    Now the footer is sticky

--- fe/src/Components/Footer.js
+++ fe/src/Components/Footer.js
@@ -4,7 +4,7 @@ import '../CSS/HomePage.css';
 function Footer(){
     return(
         <>
-        <footer class="page-footer font-small blue">
+        <footer class="page-footer font-small blue fixed-bottom">
         
           <div class="footer-copyright text-center py-3">© 2020 Differ
           </div>
