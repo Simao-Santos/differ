1efe3a5b10cfeb248cc5e67417fcaa5ffe8fbc6a
Filipe Barbosa <up201909573@fe.up.pt>
2020-10-30 19:01:13 +0000

    Update url-management.js with add_url function
    add_url function adds a new URL to the database.
    The URL is sent through a POST request.

--- be/controllers/url-management.js
+++ be/controllers/url-management.js
@@ -21,3 +21,42 @@ exports.get_urls = function(req, res, next) {
     res.send(response);
   });
 }
+
+// Add a new URL to the database
+exports.add_url = function(req, res, next) {
+  let username = 'default';
+
+  if(req.body.username)
+    username = req.body.username;
+
+  console.log(req.body);
+  console.log(req.body.url);
+
+  if(req.body.url) {
+    const queryInsert = {
+      text: 'INSERT INTO public.\"Page\" (\"Username\", \"URL\") VALUES ($1, $2)',
+      values: [username, req.body.url]
+    };
+  
+    database.query(queryInsert, (err, resultInsert) => {        
+      if(err)
+        res.send('Error adding URL');
+      else {
+        const querySelect = {
+          text: 'SELECT \"ID\", \"URL\" FROM public.\"Page\" WHERE \"Username\"=$1',
+          values: [username]
+        };
+
+        database.query(querySelect, (err, resultSelect) => {
+          if(err)
+            res.send('URL added, but couldn\'t retrieve URL list');
+          else
+            res.send(resultSelect.rows);
+        });
+      }
+    });
+  }
+  else {
+    res.send('Error: no specified URL');
+  }
+}
--- be/routes/urls.js
+++ be/routes/urls.js
@@ -4,4 +4,6 @@ var router = express.Router();
 var url_management = require('../controllers/url-management');
 
 router.get('/', url_management.get_urls);
+router.post('/add', url_management.add_url);
+
 module.exports = router;
