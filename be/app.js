d85f214d978c0ce9a6ae22bba9015f5e0f2ff668
Filipe Barbosa <up201909573@fe.up.pt>
2020-10-30 18:54:20 +0000

    Add urls.js (URL routes)
    This file will be used to handle requests to the /url path,
    that is meant for url management.
    
    The router was also loaded in app.js

--- be/app.js
+++ be/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var urlsRoute = require('./routes/urls');
 
 var app = express();
 
@@ -13,6 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// USE CORS?
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/urls', urlsRoute);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
