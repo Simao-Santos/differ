4d73bb8ac7f531b4218036ad757f62b33ec6a15e
Ricardo Pinto <ricardofpinto98@gmail.com>
2020-11-02 13:43:00 +0000

    Update the code comparison page to make it more like the visual comparison page

--- fe/src/Components/CodeComparison.js
+++ fe/src/Components/CodeComparison.js
@@ -1,27 +1,43 @@
 import React from 'react';
-import { Accordion, Card } from 'react-bootstrap';
-import '../CSS/CodeComparison.css'
+import {Accordion, Card, Button} from 'react-bootstrap';
+import '../CSS/ComparisonComponents.css'
 
 
 function CodeComparison(props) {
-    return (
-            <div className="Code-Component">
-                <h2>{props.pageName}</h2>
+    return(
+        <>
+        <div className="Comparison-Component">
+            <h2>{props.pageName}</h2>
 
-                <div className="Code-Card">
+                <div className="Comparison-Card">
+
                     <Accordion defaultActiveKey="0">
                         <Card>
-                            <Card.Body>
-                                URL IS OVER HERE
-                            </Card.Body>
+                            <Card.Header>
+                            <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                            Link ⌄
+                            </Accordion.Toggle>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="1">
+                            <Card.Body>{props.link}</Card.Body>
+                            </Accordion.Collapse>
                         </Card>
                     </Accordion>
 
-                    <div className="Code-Content">
-                        CODE DIFFERENCES ARE OVER HERE
+                    <div className="Comparison-Content">
+                    <img src={props.code1.name} className="App-logo" alt="logo" />
+                    <img src="../arrow.png" className="Arrow" alt="logo" />
+                    <img src={props.code1.name} className="App-logo" alt="logo" />
                     </div>
+
                 </div>
-            </div>
+
+                <div className="TimeStamp">
+                    <h3>{props.code1.timeStamp}</h3>
+                    <h3>{props.code1.timeStamp}</h3>
+                </div>
+        </div>        
+        </>
     );
 
 }
