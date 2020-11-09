e44dbc478d67c9945ea826e2c3484a1c0c6b5cf3
Filipe Barbosa <up201909573@fe.up.pt>
2020-11-09 17:04:37 +0000

    Update urls.js with refactored routes
    Refactor routes so they're similar to the RESTful Services specification

--- be/routes/urls.js
+++ be/routes/urls.js
@@ -4,8 +4,8 @@ var router = express.Router();
 var url_controller = require('../controllers/url-controller');
 
 router.get('/', url_controller.get_urls);
-router.post('/add', url_controller.add_url);
-router.post('/delete', url_controller.delete_url);
+router.post('/', url_controller.add_url);
+router.delete('/', url_controller.delete_url);
 router.post('/capture', url_controller.capture_url);
 router.post('/compare', url_controller.compare_url);
 
