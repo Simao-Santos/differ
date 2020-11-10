465297653d8f959b6610a57b6e5a486277a6d9c1
Andreia <andreiagouveia1@gmail.com>
2020-11-10 23:15:28 +0000

    Update VisualComparison page
     GET request still need to be fixed

--- fe/src/Pages/VisualChangesPage.js
+++ fe/src/Pages/VisualChangesPage.js
@@ -38,57 +38,74 @@ var visualData1 = [
 
 function VisualChangesPage() {
 
-  const [visualData, setData] = useState([])
   const [be_reply, setBeReply] = useState('{}')
-  const [be_urls_reply, setBeUrlsReply] = useState('{}')
+  const [be_reply_urls, setBeURLReply] = useState('{}')
 
   useEffect(() => {
     getListOfUrls() 
 
-}, []) 
-useEffect(() => {
-  console.log("be reply")
-  console.log(be_urls_reply)
-  getListOfImages()
-}, [be_urls_reply])
+  }, []) 
 
-function getListOfUrls() {
+  useEffect(() => {
+  
+    switch(be_reply.type){
+      case 'get_captures': setBeReply(be_reply.captures)
+      break
+      case 'get_captures_by_page_id': setBeReply(be_reply.captures)
+      console.log(be_reply)
+      break
+      case 'delete_capture': console.log("Delete captures")
+      break
+      case 'error': console.log('ERROR => ' + be_reply.msg)
+      break
+    }
+  
+  }, [be_reply]) 
 
+  useEffect(() => {
+    console.log(be_reply_urls)
+   
+    getListOfImages()
+  }, [be_reply_urls])
+
+  function getListOfUrls() {
+ 
+    console.log('getting urls from db')
+
+    const requestOptions = {
+      method: 'GET'
+    }
+
+    fetch("http://localhost:8000/urls/", requestOptions)
+    .then(res => res.text())
+    .then(res => setBeURLReply(JSON.parse(res).urls))
+
+  }
+
+  function getListOfImages() {
+
+    console.log('getting images from db')
+
+    const requestOptions = {
+      method: 'GET'
+    }
+
+    if(be_reply_urls.length > 0){
+      if( be_reply_urls[0].id == undefined)
+      return;
+    }
     
-
-  console.log('getting urls from db')
-
-  const requestOptions = {
-    method: 'GET'
-  }
-
-  fetch("http://localhost:8000/urls/", requestOptions)
-  .then(res => res.text())
-  .then(res => setBeUrlsReply(JSON.parse(res).urls))
-  .then(console.log("im done"))
-
-}
-
-function getListOfImages() {
-
-  console.log('getting images from db')
-
-  for(let i = 0; i < be_urls_reply.length; i++){
-
-  const requestOptions = {
-    method: 'GET'
+    for( var i=0 ; i<be_reply_urls.length ; i++){
+      console.log(be_reply_urls[i].id)
+      console.log('http://localhost:8000/captures/byPageId/' + be_reply_urls[i].id)
+    fetch('http://localhost:8000/captures/byPageId/' + be_reply_urls[i].id, requestOptions)
+    .then(res => res.text())
+    .then(res => setBeReply(JSON.parse(res)))
+    }
   }
   
-  fetch("http://localhost:8000/captures?page_id="+be_urls_reply[i].id , requestOptions)
-  .then(res => res.text())
-  .then(res => setBeReply(res))
 
-  console.log(be_reply)
 
-}
-  
-
-}
 
 
 
