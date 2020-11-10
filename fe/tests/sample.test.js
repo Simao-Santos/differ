82149ae476f45131e71807369547380b15050099
Simão Santos <simao.p.m.santos@gmail.com>
2020-11-10 22:12:26 +0000

    Update lint files

--- fe/tests/sample.test.js
+++ fe/tests/sample.test.js
@@ -1,5 +1,8 @@
+// eslint-disable-next-line
 describe('Sample Test', () => {
+// eslint-disable-next-line
   it('should test that true === true', () => {
+    // eslint-disable-next-line
     expect(true).toBe(true);
   });
 });
