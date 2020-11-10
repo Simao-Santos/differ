8e1a3298ec34a288cca6bbc8c6d982f4e63a0017
Simão Santos <simao.p.m.santos@gmail.com>
2020-11-10 21:46:26 +0000

    Update several files because of lint

--- fe/src/Components/Layout.js
+++ fe/src/Components/Layout.js
@@ -2,16 +2,15 @@ import React from 'react';
 import '../CSS/Layout.css';
 import Header from './Header';
 import Footer from './Footer';
-const Layout = ({ children }) => {
-    return (
-      <>
-        <Header />
-        <div className="App-pageContent">
-          {children}
-        </div>
-        <Footer/>
-      </>
-    )
-  }
-  
-  export default Layout  
+
+const Layout = ({ children }) => (
+  <>
+    <Header />
+    <div className="App-pageContent">
+      {children}
+    </div>
+    <Footer />
+  </>
+);
+
+export default Layout;
