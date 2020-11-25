be6432a1016e445dd8aa954e91093bc464b1e639
Andreia <andreiagouveia1@gmail.com>
2020-11-25 21:44:54 +0000

    Add route to all pages
    Now there is no need for refresh. Once someone clicks on a tab, the page is refreshed and the respective queries are asked

--- fe/src/index.js
+++ fe/src/index.js
@@ -6,46 +6,64 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Tab from 'react-bootstrap/Tab';
 import Nav from 'react-bootstrap/Nav';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import VisualChangesPage from './Pages/VisualChangesPage';
 import CodeChangesPage from './Pages/CodeChangesPage';
 import Layout from './Components/Layout';
 import UrlEdition from './Pages/UrlEdition';
 
+function getTab() {
+  switch (window.location.pathname.substring(1)) {
+    case 'urls':
+      return 'first';
+    case 'visualChanges':
+      return 'second';
+    case 'codeChanges':
+      return 'third';
+    default:
+      return 'first';
+  }
+}
+
+const tabSelected = getTab();
+
 ReactDOM.render(
   <React.StrictMode>
-    <Layout>
-      <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-        <Row>
-          <Col sm={3}>
-            <Nav variant="pills" className="flex-column">
-              <Nav.Item>
-                <Nav.Link eventKey="first">URLs</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="second">View</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="third">Code</Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </Col>
-          <Col sm={9}>
-            <Tab.Content>
-              <Tab.Pane eventKey="first">
-                <UrlEdition />
-              </Tab.Pane>
-              <Tab.Pane eventKey="second">
-                <VisualChangesPage />
-              </Tab.Pane>
-              <Tab.Pane eventKey="third">
-                <CodeChangesPage />
-              </Tab.Pane>
-            </Tab.Content>
-          </Col>
-        </Row>
-      </Tab.Container>
-    </Layout>
+    <Router>
+      <Layout>
+        <Tab.Container id="left-tabs-example" defaultActiveKey={tabSelected}>
+          <Row>
+            <Col sm={3}>
+              <Nav variant="pills" className="flex-column">
+                <Nav.Item>
+                  <Nav.Link eventKey="first" href="/urls">URLs</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second" href="/visualChanges">View</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third" href="/codeChanges">Code</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Col>
+            <Col sm={9}>
+              <Tab.Content>
+                <Tab.Pane eventKey="first">
+                  <Route path="/urls" exact component={UrlEdition} />
+                </Tab.Pane>
+                <Tab.Pane eventKey="second">
+                  <Route path="/visualChanges" exact component={VisualChangesPage} />
+                </Tab.Pane>
+                <Tab.Pane eventKey="third">
+                  <Route path="/codeChanges" exact component={CodeChangesPage} />
+                </Tab.Pane>
+              </Tab.Content>
+            </Col>
+          </Row>
+        </Tab.Container>
+      </Layout>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
