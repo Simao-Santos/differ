ba6d484f998995453d143fe0171a182f8ed6a1f8
bonecrunch5 <goncalo.valpneus@hotmail.com>
2020-11-26 02:43:57 +0000

    Added get urls from database

--- be/src/routes/diff.js
+++ be/src/routes/diff.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 var router = express.Router();
 const diff = require('../lib/diff.js');
 const { sameMsg, emptyMsg, baseText, newText, editorConfig } = require('../lib/constants.js');
+const fetch = require("node-fetch");
 
 var final=[];
 
@@ -20,23 +21,43 @@ function diffChecker(old, actual) {
   return [lc, rc, opcodes, baseTextName, newTextName];
 }
 
+async function getListOfUrls() {   
+  const myUrlIds = [];
+
+  console.log('getting urls from db')
+
+  const requestOptions = {
+    method: 'GET'
+  }
+
+  
+  var jsoncontent=[];
+  await fetch('http://localhost:8000/urls/', requestOptions)
+  .then(res => res.json())
+  .then(json => jsoncontent.push(json.urls));
+    //final.push(jsoncontent);
+    return jsoncontent;    
+}
+
 function getOne(id){
+  //TODO get page by id
   var oldpath = './public/shots/url_35_2020_11_22_01_45_53_972.html';
   var newpath = './public/shots/url_35_2020_11_22_01_52_05_736.html';
 
   var oldpage = fs.readFileSync(oldpath).toString();
   var actualpage = fs.readFileSync(newpath).toString();
 
-  var values = diffChecker(oldpage,actualpage);
+  //var values = diffChecker(oldpage,actualpage);
   return values;
 }
 
 function getValues(){
-  var values;
-  var id =35;
+  //TODO percorrer os diferentes ids presentes na base de dados
+  var jsoncontent = getListOfUrls();
 
-  var values = getOne(id);
-  final.push((values));
+
+  //var values = getOne(id);
+  //final.push((values));
 }
 
 router.get('/', function (req, res, next) {
@@ -44,11 +65,9 @@ router.get('/', function (req, res, next) {
   //diffcheker para cada "url" e dar append ao json
   //send json inteiro
 
-
   getValues();
 
   res.send(final);
-
 });
 
 module.exports = router;
