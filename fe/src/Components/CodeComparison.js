043eb6d3ad5ba2aca183111f87c9993c322fc380
Andreia <andreiagouveia1@gmail.com>
2020-12-02 11:21:22 +0000

    Add changes code structure
    Need to fix query and css

--- fe/src/Components/CodeComparison.js
+++ fe/src/Components/CodeComparison.js
@@ -4,8 +4,9 @@ import { Accordion, Card, Button } from 'react-bootstrap';
 import '../CSS/ComparisonComponents.css';
 
 const CodeComparison = (props) => {
-  const { pageName, link, code1 } = props;
-  const { name, timeStamp } = code1;
+  const {
+    pageName, link, image1, image2, timeStamp1, timeStamp2, comparison,
+  } = props;
   return (
     <>
       <div className="Comparison-Component">
@@ -18,30 +19,43 @@ const CodeComparison = (props) => {
 
         <div className="Comparison-Card">
 
-          <Accordion defaultActiveKey="0">
+          <Accordion>
             <Card>
               <Card.Header>
-                <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                <Accordion.Toggle as={Button} variant="link" eventKey="0">
                   Link ⌄
                 </Accordion.Toggle>
               </Card.Header>
-              <Accordion.Collapse eventKey="1">
+              <Accordion.Collapse eventKey="0">
                 <Card.Body>{link}</Card.Body>
               </Accordion.Collapse>
             </Card>
+            <Card>
+              <Card.Header>
+                <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                  See Differences ⌄
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="1">
+                <Card.Body>
+                  <h2>{comparison}</h2>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+
           </Accordion>
 
           <div className="Comparison-Content">
-            <img src={name} className="App-logo" alt="logo" />
+            <h2>{image1}</h2>
             <img src="../arrow.png" className="Arrow" alt="logo" />
-            <img src={name} className="App-logo" alt="logo" />
+            <h2>{image2}</h2>
           </div>
 
         </div>
 
         <div className="TimeStamp">
-          <h3>{timeStamp}</h3>
-          <h3>{timeStamp}</h3>
+          <h3>{timeStamp1}</h3>
+          <h3>{timeStamp2}</h3>
         </div>
       </div>
     </>
@@ -51,10 +65,11 @@ const CodeComparison = (props) => {
 CodeComparison.propTypes = {
   pageName: PropTypes.string.isRequired,
   link: PropTypes.string.isRequired,
-  code1: PropTypes.exact({
-    name: PropTypes.string.isRequired,
-    timeStamp: PropTypes.string.isRequired,
-  }).isRequired,
+  image1: PropTypes.string.isRequired,
+  image2: PropTypes.string.isRequired,
+  timeStamp1: PropTypes.string.isRequired,
+  timeStamp2: PropTypes.string.isRequired,
+  comparison: PropTypes.string.isRequired,
 };
 
 export default CodeComparison;
--- fe/src/Pages/CodeChangesPage.js
+++ fe/src/Pages/CodeChangesPage.js
@@ -1,57 +1,104 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../CSS/ChangesPage.css';
+import Spinner from 'react-bootstrap/Spinner';
 import CodeComparison from '../Components/CodeComparison';
+import { Pagination } from 'antd';
+import 'antd/dist/antd.css';
 
-const codeData = [
-  {
-    id: 1,
-    name: 'Page 1',
-    link: 'https://google.com',
-    timeStamp: '10/02/2020',
-    codes: [
-      {
-        timeStamp: '20/11/2020 - 17:45:00',
-        name: '../code.png',
-      }, {
-        timeStamp: '10/02/2020 - 17:45:00',
-        name: '../code.png',
-      },
-    ],
-  },
-  {
-    id: 2,
-    name: 'Page 2',
-    link: 'https://site.com/login',
-    codes: [
-      {
-        timeStamp: '20/11/2020 - 17:45:00',
-        name: '../code.png',
-
-      }, {
-        timeStamp: '10/02/2020 - 17:45:00',
-        name: '../code.png',
-      },
-    ],
-  },
-];
-
-function CodeChangesPage() {
-  return (
-    <>
-      <div className="Comparison-Cards">
-        {
-          codeData.map((ub) => (
-            <CodeComparison
-              pageName={ub.name}
-              link={ub.link}
-              code1={ub.codes[0]}
-              code2={ub.codes[1]}
-            />
-          ))
+function groupInformation(response) {
+  const test = [];
+  for (let i = 0; i < (response.length - 1); i += 1) {
+    if (response[i].id === response[i + 1].id) {
+      if (response[i].comparisonid === response[i + 1].comparisonid) {
+        if (response[i].comparisonid2 < response[i + 1].comparisonid2) {
+          response[i].complocation = response[i + 1].complocation;
         }
-      </div>
-    </>
-  );
+      } else if (response[i].comparisonid < response[i + 1].comparisonid) {
+        response[i].complocation = response[i + 1].complocation;
+      } else response[i + 1].complocation = response[i].complocation;
+      if (response[i].date > response[i + 1].date) {
+        test.push([response[i + 1], response[i]]);
+      } else test.push([response[i], response[i + 1]]);
+      i += 1;
+    }
+  }
+
+  return test;
+}
+
+class CodeChangesPage extends Component {
+  constructor(props) {
+    super(props);
+
+
+    this.state = {
+      count: 0,
+      page: 1,
+      isLoading: true,
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    console.log(this.state.page);
+    const u = (this.state.page - 1) * 20;
+    const v = this.state.page * 20;
+    const requestOptions = {
+      method: 'GET',
+    };
+    console.log(this.state.data);
+    fetch(`http://localhost:8000/captures/count`, requestOptions).then((res) => (res.clone().text())).then((res) => (this.setState((prevState) => ({
+      count: parseInt(JSON.parse(res).captures[0].count),
+    }))));
+    fetch(`http://localhost:8000/captures/${u}/${v}`, requestOptions).then((res) => (res.clone().text())).then((res) => (this.setState((prevState) => ({
+      isLoading: !prevState.isLoading,
+      data: [...prevState.data, groupInformation(JSON.parse(res).captures)],
+    }))));
+  }
+
+  onChange = page => {
+    console.log(page)
+    this.setState({
+      page: page,
+      data: [],
+      isLoading: true,
+    }, function () {
+      console.log('set state completed', this.state);
+      this.componentDidMount();
+    })
+  };
+
+  render() {
+    console.log(this.state.count);
+    const { isLoading, data } = this.state;
+    if (isLoading) {
+      return (
+        <>
+          <div className="centered">
+            <Spinner animation="border" />
+            <h2>Information is loading... Hang in there!</h2>
+          </div>
+        </>
+      );
+    }
+    return (
+      <>
+        <div className="Comparison-Cards">
+          {
+            data[0].map((ub) => (
+              <CodeComparison pageName={`Page ${ub[0].id}`} link={ub[0].url} timeStamp1={ub[0].date} timeStamp2={ub[1].date} image1={`http://localhost:8000${ub[0].image_location}`} image2={`http://localhost:8000${ub[1].image_location}`} comparison={`http://localhost:8000${ub[0].complocation}`} />
+            ))
+          }
+          <Pagination
+            total={this.state.count}
+            current={this.state.page}
+            onChange={this.onChange}
+          />
+        </div>
+
+      </>
+    );
+  }
 }
 
 export default CodeChangesPage;
